fbb6dac527dc38ca8c05224823a8edd9aa2bdcf6
Aureliano Claudio <aurelianoclaudio29@gmail.com>
2021-01-31 18:02:34 -0300

    Arquivos adicionados

---
+++ README.md
@@ -0,0 +1,4 @@
+# To Do List / Lista de Tarefas
+    -Uma simples Lista de Tarefas com funcionalidades de adicionar,marcar e eliminar deveres
+    -Feito usando HTML ,CSS e JavaScript 
+    -Projeto simples para aprendizado
---
+++ js/main.js
@@ -0,0 +1,32 @@
+let addToDoButton = document.getElementById("addToDo")
+let toDoContainer = document.getElementById("toDoContainer")
+let inputField = document.getElementById("inputField")
+let ToDoList = document.getElementById("ToDoList")
+
+addToDoButton.addEventListener('click', function () {
+    if (inputField.value) {
+        let listItem = document.createElement('li')
+        listItem.classList.add('list-item')
+        ToDoList.appendChild(listItem)
+        listItem.innerHTML =
+            `<p class='paragraph-styling'>${inputField.value}</p><button id="delete" class="deletebtn">X</button>`
+
+        inputField.value = ""
+        let paragraph = listItem.querySelector('.paragraph-styling')
+
+        paragraph.addEventListener('click', function () {
+            if (paragraph.classList.contains("line-through")) {
+                paragraph.classList.remove("line-through")
+            } else {
+                paragraph.classList.add("line-through")
+            }
+        })
+        
+        let deletebtn = listItem.querySelector('.deletebtn')
+        deletebtn.addEventListener('click', function () {
+            if (window.confirm('Deseja deletar este item?')) {
+                ToDoList.removeChild(listItem)
+            }
+        })
+    }
+})
