8f497e4da73b227d44e4479f9302901d05b3b35d
Aureliano Claudio <aurelianoclaudio29@gmail.com>
2021-04-23 14:19:59 -0300

    mudança nas variáveis e nos eventos

--- js/main.js
+++ js/main.js
@@ -1,35 +1,36 @@
-let addToDoButton = document.getElementById("addToDo")
-let toDoContainer = document.getElementById("toDoContainer")
-let inputField = document.getElementById("inputField")
-let ToDoList = document.getElementById("ToDoList")
+let addToDoButton = document.querySelector('#addToDo')
+let toDoContainer = document.querySelector('#toDoContainer')
+let inputField = document.querySelector('#inputField')
+let ToDoList = document.querySelector('#ToDoList')
 
-addToDoButton.addEventListener('click', function () {
-    if (inputField.value) {
-        let listItem = document.createElement('li')
-        listItem.classList.add('list-item')
-        ToDoList.appendChild(listItem)
-        listItem.innerHTML =
-            `<p class='paragraph-styling'>${inputField.value}</p><button id="delete" class="deletebtn">X</button>`
+addToDoButton.addEventListener('click',addListItem)
+    function addListItem() {
+        if (inputField.value) {
+            let listItem = document.createElement('li')
+            listItem.classList.add('list-item')
+            ToDoList.appendChild(listItem)
+            listItem.innerHTML =
+                `<p class='paragraph-styling'>${inputField.value}</p><button id="delete" class="deletebtn">X</button>`
 
-        inputField.value = ""
-        let paragraph = listItem.querySelector('.paragraph-styling')
+            inputField.value = ""
+            let paragraph = listItem.querySelector('.paragraph-styling')
 
-        paragraph.addEventListener('click',paragraphStyling)
-        function paragraphStyling() {
-            if (paragraph.classList.contains("line-through")) {
-                paragraph.classList.remove("line-through")
-            } else {
-                paragraph.classList.add("line-through")
+            paragraph.addEventListener('click',paragraphStyling)
+            function paragraphStyling() {
+                if (paragraph.classList.contains("line-through")) {
+                    paragraph.classList.remove("line-through")
+                } else {
+                    paragraph.classList.add("line-through")
+                }
+            }
+            
+            let deletebtn = listItem.querySelector('.deletebtn')
+
+            deletebtn.addEventListener('click',removeConfirmation)
+            function removeConfirmation() {
+                if (window.confirm('Deseja deletar este item?')) {
+                    ToDoList.removeChild(listItem)
+                }
             }
         }
-        
-        let deletebtn = listItem.querySelector('.deletebtn')
-
-        deletebtn.addEventListener('click',removeConfirmation)
-        function removeConfirmation() {
-            if (window.confirm('Deseja deletar este item?')) {
-                ToDoList.removeChild(listItem)
-            }
-        }
-    }
-})
+}
